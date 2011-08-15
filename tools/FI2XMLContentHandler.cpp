3316e44bb759881b3ed582b71a2b2d7f301862b9
Kristian Sons <kristian.sons@supporting.com>
2011-08-15 21:23:48 +0200

    Split of FI2XMLCotentHandler into header and source files

---
+++ tools/FI2XMLContentHandler.cpp
@@ -0,0 +1,67 @@
+#include "FI2XMLContentHandler.h"
+
+FI2XMLContentHandler::FI2XMLContentHandler() :
+    _stream(0)
+{
+}
+
+void FI2XMLContentHandler::setStream(std::ostream* stream)
+{
+    _stream = stream;
+}
+
+void FI2XMLContentHandler::startDocument()
+{
+    if (!_stream)
+        throw std::runtime_error("FI2XMLContentHandler: Stream is not set.");
+
+}
+
+void FI2XMLContentHandler::startElement(const FI::ParserVocabulary* vocab, const FI::Element &element, const std::vector<FI::Attribute> &attributes)
+{
+    FI::ResolvedQualifiedName elementName;
+    vocab->resolveElementName(element._qualifiedName, elementName);
+
+    *_stream << "<" << elementName;
+    // Write namespaces
+    std::vector<FI::NamespaceAttribute>::const_iterator N = element._namespaceAttributes.begin();
+    while (N != element._namespaceAttributes.end()) {
+        std::string prefix = vocab->resolvePrefix((*N)._prefix).toString();
+        *_stream << " xmlns";
+        if (!prefix.empty()) {
+            *_stream << ":" << prefix;
+        }
+        *_stream << "=\"";
+        *_stream << vocab->resolveNamespaceName((*N)._namespaceName);
+        *_stream << "\"";
+        N++;
+    }
+}
+
+void FI2XMLContentHandler::characters(const FI::ParserVocabulary* vocab, const FI::CharacterChunk &chunk)
+{
+    *_stream << vocab->resolveCharacterChunk(chunk._characterCodes);
+}
+
+void FI2XMLContentHandler::processingInstruction(const FI::ParserVocabulary* vocab, const FI::ProcessingInstruction &pi)
+{
+    *_stream << "<?" << vocab->resolveOtherNCName(pi._target) << " " << vocab->resolveOtherString(pi._content) << "?>";
+}
+
+void FI2XMLContentHandler::comment(const FI::ParserVocabulary* vocab, const FI::Comment &comment)
+{
+    *_stream << "<!--" << vocab->resolveOtherString(comment._content) << "-->";
+}
+
+void FI2XMLContentHandler::endElement(const FI::ParserVocabulary* vocab, const FI::Element &element)
+{
+    FI::ResolvedQualifiedName elementName;
+    vocab->resolveElementName(element._qualifiedName, elementName);
+    *_stream << "</" << elementName << ">";
+}
+
+void FI2XMLContentHandler::endDocument()
+{
+    //*_stream << endl << "End Document" << endl;
+}
+
--- tools/FI2XMLContentHandler.h
+++ tools/FI2XMLContentHandler.h
@@ -7,78 +7,21 @@
 #include <fi/ParserVocabulary.h>
 #include <fi/QualifiedNameTable.h>
 
+class FI2XMLContentHandler: public FI::DefaultContentHandler
+{
 
+    public:
+        FI2XMLContentHandler();
 
-class FI2XMLContentHandler: public FI::DefaultContentHandler {
+        void setStream(std::ostream* stream);
 
-public:
-	FI2XMLContentHandler() : _stream(0) {};
-
-	void setStream(std::ostream* stream) {
-		_stream = stream;
-
-	}
-
-		void startDocument() {
-			if(!_stream)
-				throw std::runtime_error("FI2XMLContentHandler: Stream is not set.");
-
-		}
-
-		void startElement(const FI::ParserVocabulary* vocab, const FI::Element &element, const std::vector<FI::Attribute> &attributes) {
-			FI::ResolvedQualifiedName elementName;
-			vocab->resolveElementName(element._qualifiedName, elementName);
-
-			*_stream << "<" << elementName;
-			// Write namespaces
-			std::vector<FI::NamespaceAttribute>::const_iterator N = element._namespaceAttributes.begin();
-			while (N != element._namespaceAttributes.end()) {
-				std::string prefix = vocab->resolvePrefix((*N)._prefix).toString();
-				*_stream << " xmlns";
-				if (!prefix.empty()) {
-					*_stream << ":" << prefix;
-				}
-				*_stream << "=\"";
-				*_stream << vocab->resolveNamespaceName((*N)._namespaceName);
-				*_stream << "\"";
-				N++;
-			}
-
-			// Write attributes
-			std::vector<FI::Attribute>::const_iterator I = attributes.begin();
-			FI::ResolvedQualifiedName attributeName;
-			while (I != attributes.end()) {
-				vocab->resolveAttributeName((*I)._qualifiedName, attributeName);
-				*_stream << " " << attributeName;
-				*_stream << "=\"" << vocab->resolveAttributeValue((*I)._normalizedValue) << "\"";
-				I++;
-			}
-			*_stream << ">";
-		}
-
-		void characters(const FI::ParserVocabulary* vocab, const FI::CharacterChunk &chunk) {
-			*_stream << vocab->resolveCharacterChunk(chunk._characterCodes);
-		}
-
-    void processingInstruction(const FI::ParserVocabulary* vocab, const FI::ProcessingInstruction &pi) {
-      *_stream << "<?" << vocab->resolveOtherNCName(pi._target) << " " << vocab->resolveOtherString(pi._content) << "?>";
-    }
-
-		void comment(const FI::ParserVocabulary* vocab, const FI::Comment &comment) {
-			*_stream << "<!--" << vocab->resolveOtherString(comment._content) << "-->";
-		}
-
-		void endElement(const FI::ParserVocabulary* vocab, const FI::Element &element) {
-			FI::ResolvedQualifiedName elementName;
-			vocab->resolveElementName(element._qualifiedName, elementName);
-			*_stream << "</" << elementName << ">";
-		}
-
-		void endDocument() {
-			//*_stream << endl << "End Document" << endl;
-		}
-	
-	std::ostream* _stream;
+        virtual void startDocument();
+        virtual void startElement(const FI::ParserVocabulary* vocab, const FI::Element &element, const std::vector<FI::Attribute> &attributes);
+        virtual void characters(const FI::ParserVocabulary* vocab, const FI::CharacterChunk &chunk);
+        virtual void processingInstruction(const FI::ParserVocabulary* vocab, const FI::ProcessingInstruction &pi);
+        virtual void comment(const FI::ParserVocabulary* vocab, const FI::Comment &comment);
+        virtual void endElement(const FI::ParserVocabulary* vocab, const FI::Element &element);
+        virtual void endDocument();
+        std::ostream* _stream;
 };
-
 
