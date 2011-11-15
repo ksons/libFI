d95219d5ccc068a071df9e00611386128b88c71b
Kristian Sons <ksons@cg.uni-saarland.de>
2011-11-15 09:19:36 +0100

    Added two more tests

--- tests/EncoderTests.cpp
+++ tests/EncoderTests.cpp
@@ -35,6 +35,35 @@ int oneNode() {
 	return 0;
 }
 
+int twoNodes() {
+	FI::Encoder e;
+	FI::StreamBitWriter writer;
+	writer.setStream(&std::cout);
+	e.setWriter(&writer);
+	e.startDocument();
+	e.startNode("root");
+	e.endNode();
+	e.startNode("root");
+	e.endNode();
+	e.endDocument();
+	return 0;
+}
+int nestedNodes() {
+	FI::Encoder e;
+	FI::StreamBitWriter writer;
+	writer.setStream(&std::cout);
+	e.setWriter(&writer);
+	e.startDocument();
+	e.startNode("node1");
+	e.startNode("node2");
+	e.startNode("node3");
+	e.endNode();
+	e.endNode();
+	e.endNode();
+	e.endDocument();
+	return 0;
+}
+
 int main(int argc, char *argv[]) {
 	if (argc < 2) {
 		std::cerr << "Running test with too few arguments." << std::endl;
@@ -44,15 +73,26 @@ int main(int argc, char *argv[]) {
 	int testNr;
 	ss >> testNr;
 
-	switch (testNr) {
-	case 0:
-		return documentOnly();
-		break;
-	case 1:
-		return oneNode();
-		break;
-	default:
-		std::cerr << "Unknown test number: " << testNr << std::endl;
+	try {
+		switch (testNr) {
+		case 0:
+			return documentOnly();
+			break;
+		case 1:
+			return oneNode();
+			break;
+		case 2:
+			return twoNodes();
+			break;
+		case 3:
+			return nestedNodes();
+			break;
+		default:
+			std::cerr << "Unknown test number: " << testNr << std::endl;
+			return 1;
+		}
+	} catch (...) {
+		std::cerr << "Unknown exeption occured: " << testNr << std::endl;
 		return 1;
 	}
 	std::cerr << "Unknown failure: " << testNr << std::endl;
